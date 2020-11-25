983c89b494e6d077ec709dc67c4338517a7a26da
denny_hsu <denny_hsu@gss.com.tw>
2020-11-25 11:35:53 +0800

    雛形

---
+++ MVCWeb/MVCWeb/Controllers/RouteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWeb.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Route
+        public ActionResult Index()
+        {
+            var result = MVCWeb.Models.Route.TempProducts.getAllproducts();
+
+
+            return View(result);
+        }
+
+        public ActionResult Index2(string id)
+        {
+            return Content(string.Format("id的值為:{0}", id));
+        }
+
+        public ActionResult Index3(string id,string page)
+        {
+            return Content(string.Format("id 的值為:{0}, page的值:{1}", id, page));
+        }
+    }
+}
